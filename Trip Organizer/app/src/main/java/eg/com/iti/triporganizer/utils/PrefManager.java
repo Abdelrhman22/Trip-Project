65d567195180e7c580c66065df985719697df6d1
Arafa <abdoarafa72@yahoo.com>
2019-04-12 16:36:44 +0200

    SharedPreferences Class

---
+++ Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/utils/PrefManager.java
@@ -0,0 +1,8 @@
+package eg.com.iti.triporganizer.utils;
+
+// SharedPreferences
+// I will use this class to check Splash screen for the first time , check if user is login before
+public class PrefManager
+{
+
+}

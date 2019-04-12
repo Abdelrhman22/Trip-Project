28c58d51bd4141d40c457c0f2f7b89f75345876f
Arafa <abdoarafa72@yahoo.com>
2019-04-12 16:33:16 +0200

    Update NetworkUtilities to check InternetConnection

--- Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/utils/NetworkUtilities.java
+++ Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/utils/NetworkUtilities.java
@@ -1,4 +1,17 @@
 package eg.com.iti.triporganizer.utils;
 
-public class NetworkUtilities {
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public final class NetworkUtilities {
+
+
+    public static boolean isOnline(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
 }

347725116de1b55f7c7293409fdf4b087c5998b5
Arafa <abdoarafa72@yahoo.com>
2019-04-13 21:06:16 +0200

    Splash Screen Added

---
+++ Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/screens/splash/SplashActivity.java
@@ -0,0 +1,14 @@
+package eg.com.iti.triporganizer.screens.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import eg.com.iti.triporganizer.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}

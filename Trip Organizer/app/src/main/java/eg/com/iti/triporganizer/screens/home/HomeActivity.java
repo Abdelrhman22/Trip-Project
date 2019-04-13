a4c89996606a4b50b83a7f2ae113f9932c8ddc76
MaiAlaa <mai.alaa.elsaeed@gmail.com>
2019-04-13 21:21:51 +0200

    Added Login Activity + Renamed MainActivity into HomeActivity

--- Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/screens/home/HomeActivity.java
+++ Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/screens/home/HomeActivity.java
@@ -1,4 +1,4 @@
-package eg.com.iti.triporganizer;
+package eg.com.iti.triporganizer.screens.home;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -13,7 +13,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+import eg.com.iti.triporganizer.R;
+
+public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override

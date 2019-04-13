ba718ecff3d5bd9fc1a095213cf5ef71140428d8
Arafa <abdoarafa72@yahoo.com>
2019-04-13 22:25:01 +0200

    Splash Screen Modified

--- Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/screens/splash/SplashActivity.java
+++ Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/screens/splash/SplashActivity.java
@@ -1,8 +1,15 @@
 package eg.com.iti.triporganizer.screens.splash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
 import eg.com.iti.triporganizer.R;
+import eg.com.iti.triporganizer.screens.home.HomeActivity;
+import eg.com.iti.triporganizer.screens.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +17,28 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImageView imageView = findViewById(R.id.logo);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
+        imageView.startAnimation(animation);
+
+        animation.setRepeatCount(1);
+
+        animation.setAnimationListener(new Animation.AnimationListener(){
+            @Override
+            public void onAnimationStart(Animation arg0) {
+            }
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+            }
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }

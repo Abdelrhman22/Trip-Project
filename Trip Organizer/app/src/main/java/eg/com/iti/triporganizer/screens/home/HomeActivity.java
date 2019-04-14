13372f292eafc1c0158c5137b10ab6fd89282e2f
Arafa <abdoarafa72@yahoo.com>
2019-04-14 22:10:32 +0200

    AddTripActivity added

--- Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/screens/home/HomeActivity.java
+++ Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/screens/home/HomeActivity.java
@@ -1,5 +1,6 @@
 package eg.com.iti.triporganizer.screens.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,6 +15,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import eg.com.iti.triporganizer.R;
+import eg.com.iti.triporganizer.screens.addTrip.AddTripActivity;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -29,8 +31,9 @@ public class HomeActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                startActivity(new Intent(HomeActivity.this, AddTripActivity.class));
             }
         });
 

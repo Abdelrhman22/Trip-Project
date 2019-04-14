0a0f4adccc8072e4601b80e9c2529658a639fc4c
salma salah mahmoud <40217644+Eng-SalmaSalah@users.noreply.github.com>
2019-04-14 19:54:47 +0200

    adding registration screen design

---
+++ Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/screens/register/RegistrationActivity.java
@@ -0,0 +1,15 @@
+package eg.com.iti.triporganizer.screens.register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import eg.com.iti.triporganizer.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}

ba841c13c7eab4a9e2b9b2af0352e3e007f47d4a
Arafa <abdoarafa72@yahoo.com>
2019-04-15 21:56:34 +0200

    add_note_raw added to be used in AddTripActcvity

--- Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/model/NoteDTO.java
+++ Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/model/NoteDTO.java
@@ -1,4 +1,33 @@
 package eg.com.iti.triporganizer.model;
 
-public class NoteDTO {
+import java.io.Serializable;
+
+public class NoteDTO implements Serializable {
+
+    // Arafa Created NoteDTO
+    /*
+    this class contains information about note like if it is checked or not , Note Content
+    which is a part of Trip as , each trip has it's own notes.
+     */
+    private boolean isChecked;
+    private String content;
+
+    public NoteDTO() {}
+
+    public NoteDTO(boolean isChecked, String content) {
+        this.isChecked = isChecked;
+        this.content = content;
+    }
+
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
--- Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/screens/addTrip/adapter/RawNotesAdapter.java
+++ Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/screens/addTrip/adapter/RawNotesAdapter.java
@@ -1,13 +1,74 @@
 package eg.com.iti.triporganizer.screens.addTrip.adapter;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import eg.com.iti.triporganizer.R;
+import eg.com.iti.triporganizer.model.NoteDTO;
+
 /*RawNotesAdapter will be used after you have added note you will see
    -------------------------------
    | Note Name      | Delete Image|
    -------------------------------
    if you want to delete this note
  */
-//expandable card view  in which we add notes
-public class RawNotesAdapter
-{
+//expandable recyclerview  in which we add notes
+public class RawNotesAdapter extends RecyclerView.Adapter<RawNotesAdapter.RawNoteHolder> {
+
+    private List<NoteDTO> mNotes;
+
+    public List<NoteDTO> getNotes() {
+        return mNotes;
+    }
+
+    public RawNotesAdapter(List<NoteDTO> tripNotes) {
+        mNotes = tripNotes;
+    }
+
+    @Override
+    public RawNoteHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.add_note_raw, parent, false);
+        return new RawNoteHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(RawNoteHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNotes.size();
+    }
+
+    class RawNoteHolder extends RecyclerView.ViewHolder {
+
+        TextView textView;
+        ImageView button;
+
+        RawNoteHolder(View v) {
+            super(v);
+            textView = v.findViewById(R.id.note_content);
+            button = v.findViewById(R.id.btn_delete);
+        }
+
+        void bind(final int index) {
+            textView.setText(mNotes.get(index).getContent());
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mNotes.remove(index);
+                    notifyDataSetChanged();
+                }
+            });
+        }
+    }
 
 }

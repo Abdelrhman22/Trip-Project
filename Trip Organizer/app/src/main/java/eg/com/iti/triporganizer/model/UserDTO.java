e6aa4cad5722084adbd57ec8d844951e2961f9fd
Arafa <abdoarafa72@yahoo.com>
2019-04-15 18:27:51 +0200

    added UserDTO Class contains Data

--- Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/model/UserDTO.java
+++ Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/model/UserDTO.java
@@ -1,4 +1,79 @@
 package eg.com.iti.triporganizer.model;
 
-public class UserDTO {
+import android.util.Patterns;
+
+// Arafa added UserDTO which contains user Details in Register Activity .
+
+public class UserDTO
+{
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+
+    public UserDTO( String name, String email, String password, String phone )
+    {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+    public UserDTO(String id, String name, String email, String password, String phone ) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public boolean isEmailValid() {
+        return Patterns.EMAIL_ADDRESS.matcher(getEmail()).matches();
+    }
+
+
+    public boolean isPasswordLengthGreaterThan5() {
+        return getPassword().length() > 5;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }

103171876e076b25ef4cc240d9748ebbcf8f8033
MaiAlaa <mai.alaa.elsaeed@gmail.com>
2019-04-17 15:10:59 +0200

    Added Package for History page and its layout files

---
+++ Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/screens/history/HistoryActivity.java
@@ -0,0 +1,15 @@
+package eg.com.iti.triporganizer.screens.history;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import eg.com.iti.triporganizer.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}

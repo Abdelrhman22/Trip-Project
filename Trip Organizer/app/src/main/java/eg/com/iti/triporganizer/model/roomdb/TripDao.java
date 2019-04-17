ae42d28d9ea800e6e89d20d16b14520678b7b0c8
Arafa <abdoarafa72@yahoo.com>
2019-04-17 14:32:51 +0200

    remove helper packages

---
+++ Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/model/roomdb/TripDao.java
@@ -0,0 +1,4 @@
+package eg.com.iti.triporganizer.model.roomdb;
+
+public interface TripDao {
+}
--- Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/screens/addTrip/AddTripPresenterImpl.java
+++ Trip Organizer/app/src/main/java/eg/com/iti/triporganizer/screens/addTrip/AddTripPresenterImpl.java
@@ -1,9 +1,5 @@
 package eg.com.iti.triporganizer.screens.addTrip;
 
-import java.util.Calendar;
-
-import eg.com.iti.triporganizer.model.helpers.roomdb.TripDTO;
-
 public class AddTripPresenterImpl
 {
 
